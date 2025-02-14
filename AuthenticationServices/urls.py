cf263e58bec3389de978d064f1a5df83c43a0141
Chandrakanta Bhoi <chandrakantabhoi10@gmail.com>
2025-02-14 10:36:00 +0530

    ci: Setup gitHub Actions to build and push docker image

--- AuthenticationServices/urls.py
+++ AuthenticationServices/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from core.views import HelthCheck
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/',include('core.urls'))
+    path('auth/',include('core.urls')),
+    path('healthcheck/',HelthCheck.as_view())
 ]
